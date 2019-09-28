d569825744d776063b5fd6853974c1bee73750d6
rex@billion <anderson.aristotle@gmail.com>
2019-09-28 13:53:01 -0400

    added bootstrap elements to button prop

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,22 +2,21 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    imageUrl: 'https://picsum.photos/200'
+    count: 0
   };
+
   render() {
     return (
-      <React.Fragment>
-        <img src="" alt=""/>
-        <span>{this.state.count}</span>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
     );
   }
 
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? "Zero"> : count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
