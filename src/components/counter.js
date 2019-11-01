d2c401b111fa8ff2321d4448034b2410eaf8c7f3
rex@billion <anderson.aristotle@gmail.com>
2019-11-01 11:13:17 -0400

    fixed function error for counter components

--- src/components/counter.js
+++ src/components/counter.js
@@ -10,25 +10,18 @@ class Counter extends Component {
   };
 
   render() {
-    console.log(this.props);
-
     return (
       <div>
-        <h4>{this.props.id}</h4>8
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <h4>{this.props.id}</h4>
+        {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span> */}
         <button
-          onClick={(this, handleIncrement)}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        <button onClick={t} className=""></button>
       </div>
     );
-  }
-
-  render() {
-    return;
   }
 }
 
